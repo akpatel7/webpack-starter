0ce3fccde7015228732d6d520d0a8e184b3eb1e6
Patel, Amitkumar <Amitkumar.Patel@tdassurance.com>
2019-09-12 17:51:52 -0400

    solution1 codility

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,31 @@
 import '../styles/index.scss';
 
 console.log('webpack starterkit');
+
+const maxZeroGap = n => {
+    var number = n;
+    var binary = '';
+    var counter = -1;
+    var max = 0;
+    while(number > 0) {
+        var digit = number % 2;
+        
+        if(digit === 1) {
+            if(counter > max) {
+                max = counter;
+            }
+            counter = 0;
+        } else if(counter >= 0) {
+            counter++;
+        }
+        
+        binary = '' + digit + binary;
+        number = parseInt(number / 2);
+    }
+    
+    //console.log('number, binary:', number, binary);
+    
+    return max;
+};
+
+console.log(maxZeroGap(9));
