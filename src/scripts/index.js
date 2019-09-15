bb2be8852cb01417d2a8b57515371079684d4021
Amit Patel <patel_amitk@hotmail.com>
2019-09-14 22:21:58 -0400

    2-2: CyclicRotaion

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -5,11 +5,30 @@ console.log('Start...');
 // you can write to stdout for debugging purposes, e.g.
 // console.log('this is a debug message');
 
-const N = 1;
+//const A = [3, 8, 9, 7, 6];
+const A = [];
+const K = 3;
 
-function solution(N) {
-    // write your code in JavaScript (Node.js 8.9.4)
-    return N;
+function rotate(A) {
+    const length = A.length;
+    let last = A[length - 1];
+    for(let i = length - 1; i >= 0; i-- ) {
+        A[i] = A[i - 1];
+    }
+    A[0] = last;
+    return A;
 }
 
-console.log(solution(N));
+function solution(A, K) {
+    // write your code in JavaScript (Node.js 8.9.4)
+    if(!A || A.length === 0 || A.length === 1) {
+        return A;
+    }
+    for(let i=0; i<K; i++) {
+        rotate(A);
+    }
+    return A;
+}
+
+console.log(A);
+console.log(solution(A, K));
